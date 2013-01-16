2482f47e843598082dde6698835e14de984e18b8
Ryan McGeary <ryan@mcgeary.org>
2013-01-16 12:48:49 -0500

    Updated which and rehash extensions to support rbx-2 versions

--- etc/rbenv.d/rehash/rbx.bash
+++ etc/rbenv.d/rehash/rbx.bash
@@ -1,2 +1,2 @@
-bin_path="${RBENV_ROOT}/versions/rbx-2.0.0-dev/gems/1.9/bin/*"
-make_shims "$bin_path"
+# make_shims ${RBENV_ROOT}/versions/rbx-2*/{1.9,1.8}/bin/*
+make_shims ../versions/rbx-2*/{1.9,1.8}/bin/*
--- etc/rbenv.d/which/rbx.bash
+++ etc/rbenv.d/which/rbx.bash
@@ -1,3 +1,3 @@
-if [ ! -x "$RBENV_COMMAND_PATH" ] && [ "$RBENV_VERSION" = "rbx-2.0.0-dev" ]; then
-  export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/gems/1.9/bin/${RBENV_COMMAND}"
+if [ ! -x "$RBENV_COMMAND_PATH" ] && [[ $RBENV_VERSION =~ rbx-2 ]]; then
+  export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/1.9/bin/${RBENV_COMMAND}"
 fi
